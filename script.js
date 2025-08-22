b35a3acc633371e4fc56d169443dfdcdb0c95ade
Diyana <shamsudheendiyana@gmail.com>
2025-08-23 01:19:09 +0530

    first commit

--- script.js
+++ script.js
@@ -27,24 +27,6 @@ document.querySelectorAll("#sidemenu li a").forEach(link => {
 });
 
 
-
-// const scriptURL = 'https://script.google.com/macros/s/AKfycbwBKc5LGDzn2-WyRoljGAxOyTFPAJtmkjh0gkO2cEE4sSUPdDFMYsqaeBTm2xyEsDcDWw/exec'
-//   const form = document.forms['submit-to-google-sheet']
-//   const msg = document.getElementById("msg")
-
-//   form.addEventListener('submit', e => {
-//     e.preventDefault()
-//     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-//       .then(response => {
-//         msg.innerHTML = "Message sent successfully"
-//         setTimeout(function(){
-//             msg.innerHTML = ""
-//         },5000)
-//         form.reset()
-//       })
-//       .catch(error => console.error('Error!', error.message))
-//   })
-
 function sendMail(){
     var params = {
         name: document.getElementById("name").value,
